ca371d1dc1bea51cb9e98164d0a4d42d41248ba3
AnchyDev <lewienato@gmail.com>
2023-10-29 11:36:44 +1100

    Added error checking and implementing appending of new config values.

--- merge.py
+++ merge.py
@@ -15,18 +15,41 @@ def main(args):
         with open(path_file_out, "rt") as file_out:
             lines = file_in.readlines()
             config_current = file_out.read()
+            line_number = 0
             for line in lines:
+                line_number = line_number + 1
+
                 # Ignore empty lines
                 if not line.strip():
+                    continue
+
+                kvp = line.split('=')
+                if len(kvp) < 2:
+                    print(f"Invalid key-value-pair found on line {line_number}.")
                     continue
 
                 key = line.split('=')[0].strip()
                 value = line.split('=')[1].strip()
 
-                config_modified = re.sub(f"{key}\s*=\s*.+", f"{key} = {value}", config_current)
+                if not key:
+                    print(f"Invalid key (empty) found on line {line_number}.")
+                    continue
+
+                if not value:
+                    print(f"Invalid value (empty) found on line {line_number}.")
+                    continue
+
+                pattern = f"{key}\s*=\s*.+"
+
+                if not re.search(pattern, config_current):
+                    print(f"Key '{key}' not found, appending to '{path_file_out}'..")
+                    config_current = f"{config_current}\n{key} = {value}"
+                    continue
+
+                config_modified = re.sub(pattern, f"{key} = {value}", config_current)
 
                 if config_modified == config_current:
-                    print(f"Failed to find key '{key}' in `{path_file_out}`, make sure your config is up-to-date.")
+                    print(f"No changes were made for '{key}', value is up-to-date.")
                     continue
                 else:
                     print(f"Merging key '{key}' into '{path_file_out}'.")
